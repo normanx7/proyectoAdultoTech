2c3b2d1140cb13fac6414dabac2ee53e4ee97d3e
normanx7 <normanx7@hotmail.com>
2025-06-21 21:42:19 +0000

    Agregando funciones al proyecto

--- proyecto.py
+++ proyecto.py
@@ -1,12 +1,8 @@
 # Talento Lab, Entrega de Proyecto - Norman Primera DNI 19102676 #25047
 
-print("\nBienvenido al Sistema de Gestión de Productos de Talento Lab")
-
 inventario_productos = []
 
-while True:
-    option_menu = input("\n!!--Menú de Opciones:--!!\n 1. Agregar producto\n 2. Mostrar productos\n 3. Buscar producto\n 4. Eliminar producto\n 5. Salir\n -. Seleccione una opción (número): ")
-    if option_menu == "1": # Agregar producto - nombre, categoría, y precio
+def agregar_producto(): # Agregar producto - nombre, categoría, y precio
         while True:
             ingresar_nombre_producto = input("Ingrese el nombre del producto: ").strip().capitalize()
             if ingresar_nombre_producto != "":
@@ -40,45 +36,60 @@ while True:
         inventario_productos.append(nuevo_producto)
         print(f"!- El producto: '{ingresar_nombre_producto}', fue ingresado al inventario.\n")
 
-    elif option_menu == "2": # Mostrar productos
-        if not inventario_productos:
-            print("Inventario vacio.\n")
-        else:
-            print("!--Productos en inventario--!")
-            for producto in inventario_productos:
-                print(f"* Producto: '{producto['nombre']}', Categoría: '{producto['categoria']}', Precio: '{producto['precio']}'.")
-
-    elif option_menu == "3": # Buscar producto
-        if not inventario_productos:
-            print("Inventario vacio, no hay productos para buscar.-\n")
-        else:
-            buscar_producto = input("Ingrese el producto que desea buscar: ").strip().capitalize()
-            en_inventario = False
-            for producto in inventario_productos:
-                if producto['nombre'] == buscar_producto:
-                    print(f" Disponible, producto: '{producto['nombre']}', categ.: '{producto['categoria']}', precio: '{producto['precio']}'.")
-                    en_inventario = True
-                    break
-            if not en_inventario:
-                print(f"El producto '{buscar_producto}' no se encuentra en el inventario.\n")
-
-    elif option_menu =="4": # Eliminar producto
-        if not inventario_productos:
-            print("Inventario vacio, no hay productos para eliminar.-\n")
-        else:
-            eliminar_producto = input("Ingrese el producto que desea eliminar: ").strip().capitalize()
-            eliminado = False
-            for i, producto in enumerate(inventario_productos):
-                if producto['nombre'] == eliminar_producto:
-                    inventario_productos.pop(i)
-                    print(f"El producto '{eliminar_producto}' fue eliminado del inventario.\n")
-                    eliminado = True
-            if not eliminado:
-                print(f"El producto '{eliminar_producto}' no existe en el inventario.\n")
-        
-    elif option_menu == "5": # Salir
-        print("Hasta pronto!")
-        break
+def mostrar_productos(): # Mostrar productos
+    if not inventario_productos:
+        print("Inventario vacio.\n")
     else:
-        print("Opción invalida, vuleva a intentarlo\n")
-        continue
+        print("!--Productos en inventario--!")
+        for producto in inventario_productos:
+            print(f"* Producto: '{producto['nombre']}', Categoría: '{producto['categoria']}', Precio: '{producto['precio']}'.")
+
+def buscar_producto(): # Buscar producto
+    if not inventario_productos:
+        print("Inventario vacio, no hay productos para buscar.-\n")
+    else:
+        buscar_producto = input("Ingrese el producto que desea buscar: ").strip().capitalize()
+        en_inventario = False
+        for producto in inventario_productos:
+            if producto['nombre'] == buscar_producto:
+                print(f" Disponible, producto: '{producto['nombre']}', categ.: '{producto['categoria']}', precio: '{producto['precio']}'.")
+                en_inventario = True
+                break
+        if not en_inventario:
+            print(f"El producto '{buscar_producto}' no se encuentra en el inventario.\n")
+
+def borrar_producto(): # Eliminar producto
+    if not inventario_productos:
+        print("Inventario vacio, no hay productos para eliminar.-\n")
+    else:
+        eliminar_producto = input("Ingrese el producto que desea eliminar: ").strip().capitalize()
+        eliminado = False
+        for i, producto in enumerate(inventario_productos):
+            if producto['nombre'] == eliminar_producto:
+                inventario_productos.pop(i)
+                print(f"El producto '{eliminar_producto}' fue eliminado del inventario.\n")
+                eliminado = True
+        if not eliminado:
+            print(f"El producto '{eliminar_producto}' no existe en el inventario.\n")
+
+def mostrar_menu():
+    while True:
+        print("\n!!--Menú de Opciones:--!!\n 1. Agregar producto\n 2. Mostrar productos\n 3. Buscar producto\n 4. Eliminar producto\n 5. Salir\n.")
+
+        opcion = input("-. Seleccione una opción (número): ")
+        if opcion == "1":
+            agregar_producto()
+        elif opcion == "2":
+            mostrar_productos()
+        elif opcion == "3":
+            buscar_producto()
+        elif opcion == "4":
+            borrar_producto()        
+        elif opcion == "5":
+            print("¡Gracias por usar el programa! ¡Chau!")
+            break
+        else:
+            print("Opción no válida. Por favor, ingresá un número del 1 al 5.\n")
+
+mostrar_menu()
+
