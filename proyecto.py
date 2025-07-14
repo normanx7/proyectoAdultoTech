cdcfca823eb7e5678fbbdd5d6d7e06a61d3a39a8
normanx7 <normanx7@hotmail.com>
2025-07-14 22:10:41 +0000

    mejorando el programa, incluyendo sqlite3, colorama

--- proyecto.py
+++ proyecto.py
@@ -1,95 +1,187 @@
-# Talento Lab, Entrega de Proyecto - Norman Primera DNI 19102676 #25047
+# Talento Lab, Entrega de Proyecto Final - Norman Primera DNI 19102676 #25047
 
-inventario_productos = []
+import sqlite3 # Importando modulo SQLITE3
 
-def agregar_producto(): # Agregar producto - nombre, categoría, y precio
-        while True:
-            ingresar_nombre_producto = input("Ingrese el nombre del producto: ").strip().capitalize()
-            if ingresar_nombre_producto != "":
-                break
-            else:
-                print("El nombre del producto NO puede estar vacío. Inténte de nuevo.\n")
+from colorama import Fore, Back, Style, init
+init() # Importando e inicializando funcionalidades de Colorama
 
-        while True:
-            ingresar_categoria_produc = input("Ingrese la categoria del prodcuto: ").strip().capitalize()
-            if ingresar_categoria_produc != "":
-                break
-            else:
-                print("La categoria del producto NO puede estar vacía. Inténte de nuevo.\n")
+# Conexión a la base de datos
+conexion = sqlite3.connect("inventario.db")
+cursor = conexion.cursor()
 
-        while True:
-            try:
-                ingresar_precio_producto = int(input("Ingrese el precio del prodcuto: "))
-                if ingresar_precio_producto > 0:
+# Crear la tabla si no existe
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS productos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        descripcion TEXT NOT NULL,
+        cantidad INTEGER NOT NULL,
+        precio REAL NOT NULL,
+        categoria TEXT NOT NULL
+    )
+''')
+conexion.commit()
+
+def registrar_producto(nombre_producto, descripcion_producto, cantidad_pruducto, precio_producto, categoria_producto):
+    """Agrega un nuevo producto al inventario"""
+    cursor.execute("INSERT INTO productos (nombre, descripcion, cantidad, precio, categoria) VALUES (?,?,?,?,?)", (nombre_producto, descripcion_producto, cantidad_pruducto, precio_producto, categoria_producto))
+    conexion.commit()
+    print(Fore.GREEN + "Producto ingresado con éxito" + Style.RESET_ALL)
+
+def mostrar_productos():
+    """Visualizar productos existentes en el inventario"""
+    cursor.execute("SELECT * FROM productos")
+    productos = cursor.fetchall()
+    print(Back.CYAN + "\n Lista de Prodcutos: " + Style.RESET_ALL)
+    for producto in productos:
+        print(f"ID: {producto[0]}, Producto: {producto[1]}, Descripción: {producto[2]}, Cantidad: {producto[3]}, Precio: $ {producto[4]:.2f}, Categoria: {producto[5]}") 
+
+def buscar_producto(id_producto):
+    """Buscar productos en el inventario, mediante su ID."""
+    cursor.execute("SELECT * FROM productos WHERE id = ?", (id_producto,))
+    producto = cursor.fetchone()
+    if producto:
+        print(Back.CYAN + "\n Prodcuto: " + Style.RESET_ALL)
+        print(f"ID: {producto[0]}, Producto: {producto[1]}, Descripción: {producto[2]}, Cantidad: {producto[3]}, Precio: $ {producto[4]:.2f}, Categoria: {producto[5]}")
+    else:
+        print(Fore.RED + "Producto no encontrado en el inventario." + Style.RESET_ALL)
+
+def buscar_producto_nombre(nombre_producto):
+    """Buscar productos en el inventario por Nombre."""
+    cursor.execute("SELECT * FROM productos WHERE nombre = ?", (nombre_producto,))
+    productos = cursor.fetchall()
+    for producto in productos:
+        print(f"ID: {producto[0]}, Producto: {producto[1]}, Descripción: {producto[2]}, Cantidad: {producto[3]}, Precio: $ {producto[4]:.2f}, Categoria: {producto[5]}")
+    else:
+        print(Fore.RED + "Producto no encontrado en el inventario." + Style.RESET_ALL)
+
+def actualizar_producto(id_producto):
+    """Actualizar datos de productos, mediante su ID."""
+    cursor.execute("SELECT * FROM productos WHERE id = ?", (id_producto,))
+    producto = cursor.fetchone()
+    if producto:
+        print(f"Producto actual: {producto}")
+        nombre = input("Nuevo nombre: ")
+        descripcion = input("Nueva descripción: ")
+        cantidad = int(input("Nueva cantidad: "))
+        precio = float(input("Nuevo precio: "))
+        categoria = input("Nueva categoría: ")
+
+        cursor.execute('''
+            UPDATE productos SET nombre = ?, descripcion = ?, cantidad = ?, precio = ?, categoria = ?
+            WHERE id = ?
+        ''', (nombre, descripcion, cantidad, precio, categoria, id_producto))
+        conexion.commit()
+        print(Fore.GREEN + "Producto actualizado exitosamente en el inventario." + Style.RESET_ALL)
+    else:
+        print(Fore.RED + "Producto no encontrado." + Style.RESET_ALL)
+
+def eliminar_producto(id_producto):
+    """Eliminar producto del inventario"""
+    cursor.execute("DELETE FROM productos WHERE id = ?", (id_producto,))
+    conexion.commit()
+    if cursor.rowcount > 0:
+        print(Fore.GREEN + "Producto eliminado del inventario." + Style.RESET_ALL)
+    else:
+        print(Fore.RED + "Producto no encontrado en el inventario." + Style.RESET_ALL)
+
+def reporte_bajo_stock(cantidad_unidades):
+    """Reporte de productos según stock"""
+    cursor.execute("SELECT * FROM productos WHERE cantidad <= ?", (cantidad_unidades,))
+    productos = cursor.fetchall()
+    if productos:
+        print(Back.CYAN + f"Los siguientes productos tienen un stock inferior o igual a {cantidad_unidades} unidades." + Style.RESET_ALL) 
+        for producto in productos:
+            
+            print(Fore.RED + f"ID: {producto[0]} | Nombre: {producto[1]} | Cantidad: {producto[3]}")
+    else:
+        print(Fore.GREEN + f"Todos los productos tienen un stock superior a {cantidad_unidades} unidades." + Style.RESET_ALL)
+
+def main(): # Menú interactivo
+    while True:
+        print(Fore.BLUE + "======Menú de Opciones======" + Style.RESET_ALL)
+        print("1. Registrar producto")
+        print("2. Ver todos los productos")
+        print("3. Buscar por ID")
+        print("4. Buscar por nombre")
+        print("5. Actualizar producto")
+        print("6. Eliminar producto")
+        print("7. Reporte de bajo stock")
+        print("8. Salir")
+
+        opcion = input("Seleccione una opción: ")
+        if opcion == "1": # Agregar producto
+            while True: 
+                nombre_producto = input("Ingrese el nombre del producto: ").strip()
+                if nombre_producto != "":
                     break
                 else:
-                    print("El precio del producto NO puede ser menor a 0. Inténte de nuevo.\n")
-            except ValueError:
-                    print("Entrada inválida. Por favor, ingrese un número para el precio.\n")
-        
-        nuevo_producto = {
-            'nombre': ingresar_nombre_producto,
-            'categoria': ingresar_categoria_produc,
-            'precio': ingresar_precio_producto
-        }
+                    print(Fore.RED + "El nombre del producto NO puede quedar vacio. " + Style.RESET_ALL)
 
-        inventario_productos.append(nuevo_producto)
-        print(f"!- El producto: '{ingresar_nombre_producto}', fue ingresado al inventario.\n")
+            while True: 
+                descripcion_producto = input("Ingrese una breve descripción del producto: ").strip()
+                if descripcion_producto != "":
+                    break
+                else:
+                    print(Fore.RED + "La descripción del producto NO puede quedar vacio. " + Style.RESET_ALL)
 
-def mostrar_productos(): # Mostrar productos
-    if not inventario_productos:
-        print("Inventario vacio.\n")
-    else:
-        print("!--Productos en inventario--!")
-        for producto in inventario_productos:
-            print(f"* Producto: '{producto['nombre']}', Categoría: '{producto['categoria']}', Precio: '{producto['precio']}'.")
+            while True:
+                try:
+                    cantidad_producto = float(input("Ingrese la cantidad producto: "))
+                    if cantidad_producto > 0:
+                                    break
+                    else:
+                        print(Fore.RED + "La cantidad del producto NO puede ser menor a 0. " + Style.RESET_ALL)
+                except ValueError:
+                        print(Fore.RED + "Entrada inválida. Por favor, ingrese un número para la cantidad.\n" + Style.RESET_ALL)
 
-def buscar_producto(): # Buscar producto
-    if not inventario_productos:
-        print("Inventario vacio, no hay productos para buscar.-\n")
-    else:
-        buscar_producto = input("Ingrese el producto que desea buscar: ").strip().capitalize()
-        en_inventario = False
-        for producto in inventario_productos:
-            if producto['nombre'] == buscar_producto:
-                print(f" Disponible, producto: '{producto['nombre']}', categ.: '{producto['categoria']}', precio: '{producto['precio']}'.")
-                en_inventario = True
-                break
-        if not en_inventario:
-            print(f"El producto '{buscar_producto}' no se encuentra en el inventario.\n")
+            while True:
+                try:
+                    precio_producto = float(input("Ingrese el precio del producto: "))
+                    if precio_producto > 0:
+                        break
+                    else:
+                        print(Fore.RED + "El precio del producto NO puede ser menor a 0. " + Style.RESET_ALL)
+                except ValueError:
+                    print(Fore.RED + "Entrada inválida. Por favor, ingrese un número para el precio.\n" + Style.RESET_ALL)
+            
+            while True: 
+                categoria_producto = input("Ingrese la categoria del producto: ").strip()
+                if categoria_producto != "":
+                    break
+                else:
+                    print(Fore.RED + "La categoria del producto NO puede quedar vacio. " + Style.RESET_ALL)
 
-def borrar_producto(): # Eliminar producto
-    if not inventario_productos:
-        print("Inventario vacio, no hay productos para eliminar.-\n")
-    else:
-        eliminar_producto = input("Ingrese el producto que desea eliminar: ").strip().capitalize()
-        eliminado = False
-        for i, producto in enumerate(inventario_productos):
-            if producto['nombre'] == eliminar_producto:
-                inventario_productos.pop(i)
-                print(f"El producto '{eliminar_producto}' fue eliminado del inventario.\n")
-                eliminado = True
-        if not eliminado:
-            print(f"El producto '{eliminar_producto}' no existe en el inventario.\n")
+            registrar_producto(nombre_producto, descripcion_producto, cantidad_producto, precio_producto, categoria_producto)
 
-def mostrar_menu():
-    while True:
-        print("\n!!--Menú de Opciones:--!!\n 1. Agregar producto\n 2. Mostrar productos\n 3. Buscar producto\n 4. Eliminar producto\n 5. Salir\n.")
-
-        opcion = input("-. Seleccione una opción (número): ")
-        if opcion == "1":
-            agregar_producto()
-        elif opcion == "2":
+        elif opcion == '2': # Mostrar todos los productos del inventario
             mostrar_productos()
-        elif opcion == "3":
-            buscar_producto()
-        elif opcion == "4":
-            borrar_producto()        
-        elif opcion == "5":
-            print("¡Gracias por usar el programa! ¡Chau!")
+        
+        elif opcion == '3': # Busca producto en el inventario por su ID
+            id_producto = input("Ingrese el ID del producto: ").strip()
+            buscar_producto(id_producto)
+
+        elif opcion == '4':  # Busca producto en el inventario por su nombre
+            nombre_producto = input("Ingrese el nombre del producto: ").strip()
+            buscar_producto(nombre_producto)
+
+        elif opcion == '5':
+            id_producto = input("Ingrese el ID del producto que desea actualizar: ").strip()
+            actualizar_producto(id_producto)
+
+        elif opcion == '6':
+            id_producto = input("Ingresá el ID del producto que desea eliminar: ").strip()
+            eliminar_producto(id_producto)
+
+        elif opcion == '7': 
+            cantidad_unidades = int(input("Ingrese la cantidad de unidades que por las que desea consultar el stock: "))
+            reporte_bajo_stock(cantidad_unidades)
+
+        elif opcion == '8':
+            print(Fore.MAGENTA + "Saliendo del sistema ¡Hasta luego!")
             break
         else:
-            print("Opción no válida. Por favor, ingresá un número del 1 al 5.\n")
+            print(Fore.RED + "Opción no válida. Intente de nuevo.")
 
-mostrar_menu()
-
+if __name__ == '__main__':
+    main()
